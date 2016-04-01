f6feef403c673952e38ef932f9ef8296b847994b
jenglish14 <jenglish14@brophybroncos.org>
2016-03-31 21:56:16 -0400

    initial menu

---
+++ financial/trunk/src/Menu.cpp
@@ -0,0 +1,24 @@
+/* @author: Josh English
+ * 
+ * @created: Mar. 31 2016
+ * @modified: Mar. 31 2016
+ *
+ * @summary: implementation of Menu Class for user IO
+*/
+
+#include<iostream>
+#include"Menu.h"
+
+using std::cout;
+using std::endl;
+
+void Menu::run()
+{
+	cout << "\n************************************************************************" << endl;
+	cout << "***************  COPYRIGHT 2016 --- Joshua English ---  ****************" << endl;
+	cout << "************************************************************************\n" << endl;
+	cout << "This program was written in 2016 as an attempt to track stock prices and" << endl;
+	cout << "trends to evaluate market movements. Data is fetched from fidelity.com; and, ownership of this data is not claimed by us. Any investment in the stock market involves risk. Use this tool at your own risk. We do not" << endl;
+	cout << "claim responsibility for lost assets.\n" << endl;
+};
+
---
+++ financial/trunk/src/Menu.h
@@ -0,0 +1,19 @@
+/* @author: Josh English
+ * 
+ * @created: Mar. 31 2016
+ * @modified: Mar. 31 2016
+ *
+ * @summary: header of Menu Class for user IO
+*/
+
+#ifndef MENU_H
+#define MENU_H
+
+class Menu{
+public:
+	void run();
+private:
+	bool m_initialSetup;
+};
+
+#endif //MENU_H
